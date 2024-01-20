7ea12fc7da07beac2bda2de622dd99edf26aa9c8
MahdiT54 <mahditanzim2@gmail.com>
2024-01-20 05:35:09 -0500

    .

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import ButtonPage from './components/ButtonPage';
+
+function App() {
+  return (
+    <div className="App">
+      <ButtonPage />
+    </div>
+  );
+}
+
+export default App;
